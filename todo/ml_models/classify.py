ac4133b24a8385add9cb260e828987575dc9a12c
ktm98 <clf17009@nitech.jp>
2021-07-07 13:38:23 +0900

    updated classify.py

--- todo/ml_models/classify.py
+++ todo/ml_models/classify.py
@@ -1,25 +1,34 @@
 import numpy as np
 import gensim
-
+from janome.tokenizer import Tokenizer
+# from nltk.corpus import stopwords
 import os
 # os.system('ls')
 # os.system('ls ../')
 
-MODEL = gensim.models.KeyedVectors.load_word2vec_format('entity_vector/entity_vector.model.bin', binary=True)
+MODEL = gensim.models.KeyedVectors.load_word2vec_format('https://github.com/gotaku6629/todo_classification/releases/download/v0.1/entity_vector.model.bin', binary=True)
 CATEGORY_LIST = ('勉強', '授業', '研究', '遊び', '旅行', '就活')
+TOKENIZER = Tokenizer()
+# STOPWORDS = stopwords.words('japanese')
+
 def classify(text):
-    print(text)
+    print('text: ', text)
+    token = TOKENIZER.tokenize(text)
+    texts = [w.surface for w in token if w.part_of_speech[0] in ('名', '動', '形')]
+    # texts = [w.surface for w in token if w not in STOPWORDS]
+
+    print('texts: ', texts)
     # try:
-    similarities = [MODEL.similarity(text, c) for c in CATEGORY_LIST]
-    print(similarities)
-    category = CATEGORY_LIST[np.argmax(similarities)]
+    similarities = np.array([[MODEL.similarity(text, c) for c in CATEGORY_LIST] for text in texts])
+    print('similarites: ', similarities)
+    category = CATEGORY_LIST[np.argmax(np.max(similarities, axis=0))]
     
     return category
     # except KeyError:
     #     return 'その他'
 
 
-    if len(text) > 3:
-        return '勉強'
-    else:
-        return '遊び'
+    # if len(text) > 3:
+    #     return '勉強'
+    # else:
+    #     return '遊び'
